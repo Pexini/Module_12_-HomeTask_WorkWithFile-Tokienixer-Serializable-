f9a22b06e17cc53b6fa642c2a20f50e8677b37e7
Pavel Davydov <Davydov.works@gmail.com>
2024-06-05 04:04:20 -0400

    add task_Easy

--- src/main/java/Easy/PrintExample.java
+++ src/main/java/Easy/PrintExample.java
@@ -1,4 +1,12 @@
 package Easy;
 
+import java.io.*;
+import java.time.LocalDateTime;
+
 public class PrintExample {
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintWriter printWriter = new PrintWriter("FileForProgram/LocalTime_result.txt");
+        printWriter.println(LocalDateTime.now());
+        printWriter.close();
+    }
 }
