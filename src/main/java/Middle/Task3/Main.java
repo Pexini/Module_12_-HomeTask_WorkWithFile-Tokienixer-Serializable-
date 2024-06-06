aeec12ab64a58cc9c5c485ea961babf0915506c9
Pavel Davydov <Davydov.works@gmail.com>
2024-06-06 04:38:08 -0400

    add Middle_Task3

---
+++ src/main/java/Middle/Task3/Main.java
@@ -0,0 +1,57 @@
+package Middle.Task3;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        File in = new File("FileForProgram/Middle_Files/Task_3_in");
+        File out = new File("FileForProgram/Middle_Files/Task_3_out");
+
+        try (
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(in)));
+                PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(out)))
+        ) {
+//            List<String> lines = new ArrayList<>();                                Переворачиваем и строчки местами и слова
+//            String line;
+//            while ((line = bufferedReader.readLine()) != null) {
+//                StringBuilder reversedText = new StringBuilder(line);
+//                reversedText.reverse();
+//                lines.add(reversedText.toString());
+//            }
+//            for (int i = lines.size() - 1; i >= 0; i--) {
+//                printWriter.println(lines.get(i));
+//            }
+            String line;
+            // Считываем строки из исходного файла
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                // Разбиваем строку на слова
+                for (int i = 0; i < words.length; i++) {
+                    words[i] = reverseWord(words[i]);
+                }
+                StringBuilder reversedeLine = new StringBuilder();
+                for (String word : words) {
+                    reversedeLine.append(word).append(" ");
+                }
+                // Записываем перевернутую строку в результирующий файл
+                printWriter.println(reversedeLine.toString().trim());
+            }
+
+            System.out.println("Файл успешно перевернут.");
+        } catch (IOException e) {
+            System.out.println("Поврежденный файл");
+
+        }
+
+    }
+
+    public static String reverseWord(String word) {
+        return new StringBuilder(word).reverse().toString();
+    }
+}
+
+
+
