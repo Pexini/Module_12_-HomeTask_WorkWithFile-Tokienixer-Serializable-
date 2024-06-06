c1c57debf334fe3a6481b7d81483df0c458608b3
Pavel Davydov <Davydov.works@gmail.com>
2024-06-06 02:39:06 -0400

    add Middle_Task2_PrintNumbers

---
+++ src/main/java/Middle/Task2/Main_Numbers.java
@@ -0,0 +1,9 @@
+package Middle.Task2;
+
+public class Main_Numbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <=100; i++) {
+            System.out.println(i);
+        }
+    }
+}
