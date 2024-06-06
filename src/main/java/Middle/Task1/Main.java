104a02f806cfe19bb73693f66fd3ce95824dda1e
Pavel Davydov <Davydov.works@gmail.com>
2024-06-06 02:27:06 -0400

    add Middle_Task1_v2

--- src/main/java/Middle/Task1/Main.java
+++ src/main/java/Middle/Task1/Main.java
@@ -1,24 +1,34 @@
 package Middle.Task1;
 
+
 import java.io.*;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName()); // попробовал использовать логгер в коде
+
     public static void main(String[] args) throws Exception {
         File read = new File("FileForProgram/File_Middle_Task1");
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(read)));
-        bufferedReader.lines().forEach(x -> {
-            String[] arrayString = x.split(" ");
-
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(read)));) {
             Map<String, Integer> wordMap = new HashMap<>();
-            for (String str : arrayString) {
-                wordMap.put(str, wordMap.getOrDefault(str, 0) + 1);
-            }
+
+
+            bufferedReader.lines().forEach(text -> {
+                String cleanedLine = text.replaceAll("[^a-zA-Zа-яА-Я0-9\\s]", "").toLowerCase();
+                String[] arrayString = cleanedLine.split("\\s+");
+
+                for (String str : arrayString) {
+                    wordMap.put(str, wordMap.getOrDefault(str, 0) + 1);
+                }
+            });
+
             Map<String, Integer> sortedMap = wordMap.entrySet()
                     .stream()
                     .sorted(
@@ -29,13 +39,13 @@ public class Main {
                                     Map.Entry::getKey,
                                     Map.Entry::getValue,
                                     (e1, e2) -> e1,
-                            LinkedHashMap::new
-                    ));
-            sortedMap.forEach((str, count) -> System.out.println( str + "[" + count + "]"));
+                                    LinkedHashMap::new
+                            ));
+            sortedMap.forEach((str, count) -> System.out.println(str + "[" + count + "]"));
 
-
-        });
-        bufferedReader.close();
-
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error reading the file", e);
+        }
     }
+
 }
