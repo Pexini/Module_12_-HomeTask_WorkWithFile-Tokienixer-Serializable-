ef62f00f8316c114227bb439fcffe9d55f410204
Pavel Davydov <Davydov.works@gmail.com>
2024-06-07 04:11:35 -0400

    add Middle_Task4

--- src/main/java/Middle/Task4/Main.java
+++ src/main/java/Middle/Task4/Main.java
@@ -5,17 +5,26 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         File in = new File("FileForProgram/Middle_Files/Task_4_in");
-        File out = new File("FileForProgram/Middle_Files/Task_4_ out");
+        File out = new File("FileForProgram/Middle_Files/Task_4_ out_WithMatches");
+//        File out = new File("FileForProgram/Middle_Files/Task_4_ out_WithReplaceAll");
 
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(in)));
              PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(out)))
         ) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                String withoutNumbers = line.replace("\\d+", "");
-                printWriter.println(bufferedReader);
+//                String withoutNumbers = line.replaceAll("\\d+", "");                    Удаление цифр с помощью ReplaceAll
+//                printWriter.println(withoutNumbers);
+
+                StringBuilder withoutNumbers = new StringBuilder();
+                for (char ch : line.toCharArray()) {
+                    if (!Character.toString(ch).matches("\\d")) {
+                        withoutNumbers.append(ch);
+                    }
+                }
+                printWriter.println(withoutNumbers);
             }
-            System.out.println("Numbers was deleted" + w);
+            System.out.println("Numbers was deleted");
         } catch (IOException e) {
             System.out.println("Something was wrong");
         }
