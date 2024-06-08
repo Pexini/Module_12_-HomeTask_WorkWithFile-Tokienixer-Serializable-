451110e3455ecb98539c2ef8d40991d9bbc40d1c
Pavel Davydov <Davydov.works@gmail.com>
2024-06-08 11:33:49 -0400

    Task 1 with flatMap

--- src/main/java/Middle/Task1/Main.java
+++ src/main/java/Middle/Task1/Main.java
@@ -2,10 +2,7 @@ package Middle.Task1;
 
 
 import java.io.*;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -18,36 +15,53 @@ public class Main {
         File read = new File(FILE_PATH);
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(read));) {
-            Map<String, Integer> wordMap = new HashMap<>();
+            //Метод с использованием FlatMap
+            Map<String, Long> wordMap = bufferedReader.lines()
+                    .flatMap(lines -> Arrays.stream(lines.replaceAll("[^a-zA-Zа-яА-Я0-9\\s]", "").toLowerCase().split("\\s+")))
+                    .filter(word -> !word.isEmpty())
+                    .collect(Collectors.groupingBy(word -> word, Collectors.counting()));
 
-
-            bufferedReader.lines().forEach(text -> {
-                String cleanedLine = text.replaceAll("[^a-zA-Zа-яА-Я0-9\\s]", "").toLowerCase();
-                String[] arrayString = cleanedLine.split("\\s+");
-
-                for (String str : arrayString) {
-                    if (!str.isEmpty()) {                                     // добавил проверку на пустую строку, так как без нее выпадает две пустые строки
-                        wordMap.put(str, wordMap.getOrDefault(str, 0) + 1);
-                    }
-                }
-            });
-
-            Map<String, Integer> sortedMap = wordMap.entrySet()
+            Map<String, Long> sortedMap = wordMap.entrySet()
                     .stream()
-                    .sorted(
-                            Map.Entry.<String, Integer>comparingByValue
-                                    (Comparator.reverseOrder()))
-                    .collect(
-                            Collectors.toMap(
-                                    Map.Entry::getKey,
-                                    Map.Entry::getValue,
-                                    (e1, e2) -> e1,
-                                    LinkedHashMap::new
-                            ));
+                    .sorted(Map.Entry.<String, Long>comparingByValue(Comparator.reverseOrder()))
+                    .collect(Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue,
+                            (e1, e2) ->e1,
+                            LinkedHashMap:: new
+                    ));
             sortedMap.forEach((str, count) -> System.out.println(str + "[" + count + "]"));
 
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Error reading the file", e);
+
+            //Метод с использованием HashMap
+//            Map<String, Integer> wordMap = new HashMap<>();
+//
+//
+//            bufferedReader.lines().forEach(text -> {
+//                String cleanedLine = text.replaceAll("[^a-zA-Zа-яА-Я0-9\\s]", "").toLowerCase();
+//                String[] arrayString = cleanedLine.split("\\s+");
+//
+//                for (String str : arrayString) {
+//                    if (!str.isEmpty()) {                                     // добавил проверку на пустую строку, так как без нее выпадает две пустые строки
+//                        wordMap.put(str, wordMap.getOrDefault(str, 0) + 1);
+//                    }
+//                }
+//            });
+//
+//            Map<String, Integer> sortedMap = wordMap.entrySet()
+//                    .stream()
+//                    .sorted(
+//                            Map.Entry.<String, Integer>comparingByValue
+//                                    (Comparator.reverseOrder()))
+//                    .collect(
+//                            Collectors.toMap(
+//                                    Map.Entry::getKey,
+//                                    Map.Entry::getValue,
+//                                    (e1, e2) -> e1,
+//                                    LinkedHashMap::new
+//                            ));
+//            sortedMap.forEach((str, count) -> System.out.println(str + "[" + count + "]"));
+
         }
     }
 
