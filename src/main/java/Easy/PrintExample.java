af45a1d59b9b0013cc7e6847720bee87b1a6d1de
Pavel Davydov <Davydov.works@gmail.com>
2024-06-08 10:54:44 -0400

    add Task_Easy add private static

--- src/main/java/Easy/PrintExample.java
+++ src/main/java/Easy/PrintExample.java
@@ -4,8 +4,10 @@ import java.io.*;
 import java.time.LocalDateTime;
 
 public class PrintExample {
+    private static final String FILE  = "FileForProgram/File_Easy/LocalTime_result.txt";
+
     public static void main(String[] args) throws FileNotFoundException {
-        PrintWriter printWriter = new PrintWriter("FileForProgram/File_Easy/LocalTime_result.txt");
+        PrintWriter printWriter = new PrintWriter(FILE);
         printWriter.println(LocalDateTime.now());
         printWriter.close();
     }
