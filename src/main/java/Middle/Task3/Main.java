78fb5c5ec1ac61eb5e6e63f00111b3cd435d0e7c
Pavel Davydov <Davydov.works@gmail.com>
2024-06-13 03:24:49 -0400

    Change task 3

--- src/main/java/Middle/Task3/Main.java
+++ src/main/java/Middle/Task3/Main.java
@@ -3,6 +3,7 @@ package Middle.Task3;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 
 public class Main {
@@ -12,12 +13,13 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            String content = Files.readString(Paths.get(IN));
-            String reversedContent = new StringBuilder(content).reverse().toString();
+            List<String> lines = Files.readAllLines(Paths.get(IN));
             try (PrintWriter printWriter = new PrintWriter(new File(OUT))) {
-                printWriter.println(reversedContent);
+                for (String line : lines) {
+                    String reversLine = new StringBuilder(line).reverse().toString();
+                    printWriter.println(reversLine);
+                }
             }
-
             System.out.println("Файл успешно перевернут.");
         } catch (IOException e) {
             System.out.println("Wrong");
